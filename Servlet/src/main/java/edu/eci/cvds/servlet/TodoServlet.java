bfbd600dfcc72a49c8bfeabfde0ac8cd3770ff65
TianRojas <62759668+Tianrojas@users.noreply.github.com>
2022-10-17 17:51:22 -0500

    v 3.0

--- Servlet/src/main/java/edu/eci/cvds/servlet/TodoServlet.java
+++ Servlet/src/main/java/edu/eci/cvds/servlet/TodoServlet.java
@@ -42,4 +42,26 @@ public class TodoServlet extends HttpServlet {
         }
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp){
+        try {
+            Writer responseWriter = resp.getWriter();
+            Optional<String> optID = Optional.ofNullable(req.getParameter("id"));
+            String id = optID.isPresent() && !optID.get().isEmpty() ? optID.get() : "";
+            Todo todo = Service.getTodo(Integer.parseInt(id));
+            resp.setStatus(HttpServletResponse.SC_OK);
+            ArrayList<Todo> todos= new ArrayList<Todo>();
+            todos.add(todo);
+            responseWriter.write(Service.todosToHTMLTable(todos));
+            responseWriter.flush();
+        } catch (NumberFormatException nex) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } catch (MalformedURLException mex) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        } catch (FileNotFoundException fex) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
 }
